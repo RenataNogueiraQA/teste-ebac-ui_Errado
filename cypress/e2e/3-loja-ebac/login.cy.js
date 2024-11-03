32c22d79bc8eb6d4f58ae3aa561883776de85980
Renata Nogueira <re.nogueira@yahoo.com.br>
2024-11-03 15:15:36 -0300

    Trabalhando com Fixture

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,20 +1,21 @@
 /// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
+
 
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
     afterEach(() => {
         cy.screenshot()
     });
 
-    it ('Deve fazer login com sucesso', () => {        
+    it('Deve fazer login com sucesso', () => {        
         cy.get('#username').type('renata.teste@teste.com.br')
         cy.get('#password').type('Teste123@')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, renata.teste (não é renata.teste? Sair)')
     })
 
@@ -31,5 +32,21 @@ describe('Funcionalidade: Login', () => {
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail renata.teste@teste.com.br está incorreta.')
         cy.get('.woocommerce-error').should('exist')
+    });
+
+    it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, renata.teste1 (não é renata.teste1? Sair)')
+    });
+
+    it.only('Deve fazer login com sucesso - Usando Fixture', () => {
+        cy.fixture('perfil').then( dados => {
+            cy.get('#username').type(dados.usuario, {log:false})
+            cy.get('#password').type(dados.senha, {log:false})
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, renata.teste1 (não é renata.teste1? Sair)')
+        })
     });
 })
